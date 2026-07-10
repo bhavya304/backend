6c53c167d7b72fbd4b69c7dffffc9a90d093dcd7
bhavya304 <1602-23-733-180@vce.ac.in>
2026-07-10 11:47:17 +0530

    features:(CRUD)

--- src/controller/post.controller.js
+++ src/controller/post.controller.js
@@ -1,9 +1,9 @@
-import {Post} from "../models/post.model.js";
+import { Post } from "../models/post.model.js";
 
 const createPost = async (req, res) => {
     try {
         const { name, description, age } = req.body;
-        if(!name || !description || !age) {
+        if (!name || !description || !age) {
             return res.status(400).json({ message: "All fields are required" });
         }
         const post = await Post.create({ name, description, age });
@@ -23,7 +23,39 @@ const getPosts = async (req, res) => {
         res.status(500).json({ message: "Internal server error", error: error.message });
     }
 }
+
+const updatePost = async (req, res) => {
+    try {
+        //validation
+        if (Object.keys(req.body).length === 0) {
+            return res.status(400).json({ message: "Request body is empty" });
+        }
+        const post = await Post.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!post) {
+            return res.status(404).json({ message: "Post not found" });
+        }
+        res.status(200).json({ message: "Post updated successfully", post });
+    } catch (error) {
+        console.error("Update post error:", error);
+        res.status(500).json({ message: "Internal server error", error: error.message });
+    }
+}
+
+const deletePost = async (req, res) => {
+    try {
+        const deleted = await Post.findByIdAndDelete(req.params.id);
+        if(!deleted) {
+            return res.status(404).json({ message: "Post not found" });
+        }
+        return res.status(200).json({ message: "Post deleted successfully", deleted });
+    } catch (error) {
+        console.error("Delete post error:", error);
+        res.status(500).json({ message: "Internal server error", error: error.message });
+    }
+}
 export {
     createPost,
-    getPosts
+    getPosts,
+    updatePost,
+    deletePost
 }
--- src/routes/post.route.js
+++ src/routes/post.route.js
@@ -1,6 +1,10 @@
 import express from "express";
-import {createPost,getPosts} from "../controller/post.controller.js";
+import {createPost,getPosts,updatePost,deletePost} from "../controller/post.controller.js";
 const router=express.Router();
+
 router.post("/create",createPost);
 router.get("/getPosts",getPosts);
+router.put("/update/:id",updatePost);
+router.delete("/delete/:id",deletePost);
+
 export default router;
